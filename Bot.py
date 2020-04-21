55848324760d07de9d5cdc20a6d6c8070c9ba8f9
Вадим <ale244453942008@yandex.ru>
2020-04-21 19:37:05 +0300

    ‘rabotaidavainypojalysta’

--- Bot.py
+++ Bot.py
@@ -1,6 +1,22 @@
 import telebot
+#
+
+from telebot import types
+
+
+keyboardStart = types.InlineKeyboardMarkup(row_width=2)
+keyRU = types.InlineKeyboardButton(text='Русский', callback_data='Ru')
+keyEng = types.InlineKeyboardButton(text='English', callback_data='Eng')
+keyboardStart.add(keyRU, keyEng)
+
+keyboardTimezone = types.ReplyKeyboardMarkup(resize_keyboard=True)
+buttonKtz = types.KeyboardButton('Отправить свою локацию', request_location=True)
+keyboardTimezone.add(buttonKtz)
+
+
+#
 from config import TOKEN
-from keyboard import keyboardStart, keyboardTimezone
+#from keyboard import keyboardStart, keyboardTimezone
 
 bot = telebot.TeleBot(TOKEN)
 
